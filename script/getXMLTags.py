8ca3e262ce50c92fc2f3d5b635818e9c2b416a6b
jessicaLarsson <jessicalarsson1987@gmail.com>
2015-04-22 16:10:18 +0200

    fixed script

--- script/getXMLTags.py
+++ script/getXMLTags.py
@@ -2,6 +2,7 @@
 # you need to provide the carData.txt file as input
 # use from console: python getXMLTags.py carData.txt
 from sys import argv
+import string
 
 script, filename = argv
 
@@ -12,26 +13,20 @@ idealTrack = "<idealTrack>\n"
 
 l = 0
 p = 0
-b = 0
 with open(filename) as f:
 	for line in f:
-		line = line.split()
+		
 		l = l+1
 		if l > 4:
+			line = line.replace("\n", "")
 			newFile.write("\n<point id='IdealPoint.")
-			b = 0
 			p = p+1
 			idealTrack = idealTrack + "<point ref='IdealPoint." + str(p).zfill(4) + "'/>"
 			newFile.write(str(p).zfill(4) + "'><translation><vector jtype='java_lang_Float' size='3'><entry>")
-			for char in line:
-				if char==":":
-					b = b+1
-					if b>1:
-						newFile.write("</entry>")
-					newFile.write("<entry>")
-				else:
-					newFile.write(char)
-			newFile.write("</entry></vector></translation></point>")
+			for char in ':':
+				line = line.replace(char, "</entry><entry>")
+			
+			newFile.write(line + "</entry></vector></translation></point>")
 
 
 
